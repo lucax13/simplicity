5b25461de8ff940729372804b57d0d3d12555ab1
lucax13 <lucas13lima2006@gmail.com>
2024-10-16 11:25:23 -0300

    final contato

--- js/contato.js
+++ js/contato.js
@@ -67,6 +67,37 @@ else{
 }
 
 // final do evento botao
-
-
 });
+
+// codigo do formspree
+
+var form = document.getElementById("my-form");
+  
+  async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+          'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        status.innerHTML = "mensagem enviada com sucesso!";
+        form.reset()
+      } else {
+        response.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+          } else {
+            status.innerHTML = "Oops! algo de errado nao esta certo"
+          }
+        })
+      }
+    }).catch(error => {
+      status.innerHTML = "Oops! tente novamente"
+    });
+  }
+  form.addEventListener("submit", handleSubmit)
